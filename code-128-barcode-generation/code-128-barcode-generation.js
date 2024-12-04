36b1552d13dd16341141799f179dc7ede0cc8595
cnbilinyj <wjrbilibilinyj@163.com>
2024-12-04 17:32:55 +0800

    add code-128-barcode-generation fonction

--- code-128-barcode-generation/code-128-barcode-generation.js
+++ code-128-barcode-generation/code-128-barcode-generation.js
@@ -1,4 +1,4 @@
-function code128generator(data, canvas) {
+function code128generator(data, canvas, printText) {
 	let renderElements = "";
 	// 校验位的第一步
 	let checkDigitCalculationSum = 0;
@@ -7,6 +7,8 @@ function code128generator(data, canvas) {
 	let Code128BarPattern = ["11011001100","11001101100","11001100110","10010011000","10010001100","10001001100","10011001000","10011000100","10001100100","11001001000","11001000100","11000100100","10110011100","10011011100","10011001110","10111001100","10011101100","10011100110","11001110010","11001011100","11001001110","11011100100","11001110100","11101101110","11101001100","11100101100","11100100110","11101100100","11100110100","11100110010","11011011000","11011000110","11000110110","10100011000","10001011000","10001000110","10110001000","10001101000","10001100010","11010001000","11000101000","11000100010","10110111000","10110001110","10001101110","10111011000","10111000110","10001110110","11101110110","11010001110","11000101110","11011101000","11011100010","11011101110","11101011000","11101000110","11100010110","11101101000","11101100010","11100011010","11101111010","11001000010","11110001010","10100110010","10100001100","10010110000","10010000110","10000101100","10000100110","10110010000","10110000100","10011010000","10011000010","10000110100","10000110010","11000010010","11001010000","11110111010","11000010100","10001111010","10100111100","10010111100","10010011110","10111100100","10011110100","10011110010","11110100100","11110010100","11110010010","11011011110","11011110110","11110110110","10101111000","10100011110","10001011110","10111101000","10111100010","11110101000","11110100010","10111011110","10111101110","11101011110","11110101110","11010000100","11010010000","11010011100"];
 	let stopBar = "1100011101011";
 	let startPatternIndex = {"a": 103, "b": 104, "c": 105};
+
+	let code128CombinationArr = [];
 
 	/*
 	三个临时数组，
@@ -33,17 +35,20 @@ function code128generator(data, canvas) {
 		let currentRenderElements = "";
 		let currentForCharList = "";
 		let index = 0;
+		let currentString = "";
 		if ((/\d{4}/.test(data.substr(i, 4)) && currentCharList != "c") || (/\d{2}/.test(data.substr(i, 2)) && (i == 0 || currentCharList == "c"))) {
-			index = char.c.indexOf(data.substr(i, 2));
+			currentString = data.substr(i, 2);
+			index = char.c.indexOf(currentString);
 			currentRenderElements = Code128BarPattern[index];
 			currentForCharList = "c";
 			i ++;
 		} else {
 			currentForCharList = currentCharList == "a" ? "a" : "b";
-			index = char[currentForCharList].indexOf(data[i]);
+			currentString = data[i];
+			index = char[currentForCharList].indexOf(currentString);
 			if (index == -1) {
 				currentForCharList = currentForCharList == "a" ? "b" : "a";
-				index = char[currentForCharList].indexOf(data[i])
+				index = char[currentForCharList].indexOf(currentString)
 			}
 			if (index == -1) {
 				throw new RangeError("Parameter \"data\" (parameter 1) contains unsupported characters.");
@@ -63,9 +68,11 @@ function code128generator(data, canvas) {
 			let checkDigitCalculationIncrease = index * checkDigitWeight;
 			checkDigitCalculationSum += checkDigitCalculationIncrease;
 			console.log("更改编码集至Code %s，编码集起始绘制元%s，增加校验位计算和%s", currentForCharList, Code128BarPattern[startPatternIndex[currentForCharList]], checkDigitCalculationIncrease);
+			code128CombinationArr.push(`[${currentForCharList}]`);
 		}
 		console.log("当前字符绘制元%s，增加校验位计算和%s", currentRenderElements, checkDigitCalculationIncrease);
 		renderElements += currentRenderElements;
+		code128CombinationArr.push(currentString);
 	}
 	let checkDigit = checkDigitCalculationSum % 103;
 	renderElements += Code128BarPattern[checkDigit];
@@ -75,18 +82,26 @@ function code128generator(data, canvas) {
 
 	if (canvas instanceof Element && canvas.tagName == "CANVAS") {
 		let width = (renderElements.length * 5) + 50;
-		let heigth = 220;
+		let height = 220;
 		let x = 25;
 		let step = 5;
 		canvas.width = width;
-		canvas.heigth = heigth;
+		canvas.height = height;
 		let ctx = canvas.getContext('2d');
 		ctx.fillStyle = "#ffffff";
-		ctx.fillRect(-1, -1, width + 2, heigth + 2);
+		ctx.fillRect(-1, -1, width + 2, height + 2);
 		ctx.fillStyle = "#000000";
+		ctx.font = "25px monospace"
+		if (printText) {
+			for (index in code128CombinationArr) {
+				const item = code128CombinationArr[index];
+				const textX = x + step * 11 * index;
+				ctx.fillText(item, textX, height - 10)
+			}
+		}
 		for (i of renderElements) {
 			if (i == '1') {
-				ctx.fillRect(x, 0, step, heigth - 25);
+				ctx.fillRect(x, 10, step, height - (printText ? 50 : 20));
 			}
 			x += step;
 		}
